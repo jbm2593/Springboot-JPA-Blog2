5140b9dfa4b7abfd82d27927e8c07f718c8c2517
jbm2593 <qudan2593@naver.com>
2022-07-18 23:20:27 +0900

    로그인 구현

--- src/main/java/com/cos/blog2/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog2/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog2.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +19,26 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+//	@Autowired
+//	private HttpSession session; //이렇게 추가해서 해도 됌.
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { //username, password, email
 			System.out.println("UserApiController : save 함수 호출됨");
 			user.setRole(RoleType.USER);
 			userService.회원가입(user);
 			 return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
+	}
+	
+	@PostMapping("api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
+		System.out.println("UserApiController : login 호출됨");
+		User principal = userService.로그인(user); //principal (접근주체)
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
 }
--- src/main/java/com/cos/blog2/repository/UserRepository.java
+++ src/main/java/com/cos/blog2/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cos.blog2.model.User;
@@ -9,5 +10,13 @@ import com.cos.blog2.model.User;
 //자동으로 bean 등록이 된다.
 @Repository //생략 가능
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	//JPA Naming 전략
+	
+	// SELECT * FROM user WHERE username = 파라미터1  AND password = 파라미터2;
+	User findByUsernameAndPassword(String username, String password);
+	
+	
+	// UserRepository.login 을 하면 아래 쿼리가 실행되고 리턴은 User 객체로 해줌!
+//	@Query(value="SELECT * FROM user WHERE username = ?1 AND password =?2", nativeQuery = true)
+//	User login(String username, String password);
 }

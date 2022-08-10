ba30725bf1e1a803b3df9bd1e625a6e8da5ae256
jbm2593 <qudan2593@naver.com>
2022-08-10 21:56:10 +0900

    글목록보기

--- src/main/java/com/cos/blog2/controller/BoardController.java
+++ src/main/java/com/cos/blog2/controller/BoardController.java
@@ -3,17 +3,26 @@ package com.cos.blog2.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cos.blog2.config.auth.PrincipalDetail;
+import com.cos.blog2.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
+	
+	
+		// 컨트롤러에서 세션을 어떻게 찾는지?
 		@GetMapping({"", "/"})
-		public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
+		public String index(Model model) { 
+			model.addAttribute("boards", boardService.글목록());
 			//   WEB-INF/views//WEB-INF/views/index.jsp
-				return "index";			
+				return "index"; // viewResolver 작동!!
 		}
 		
 		// USER 권한이 필요
